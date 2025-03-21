c553f899b5d7fc5cf47fde0adf2dc23a7cc3604f
rohit_attaluri552 <rohith.attaluri@biofourmis.com>
2025-03-22 02:13:41 +0530

    [0]: Enhance side nav content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,26 @@ import { AuthService } from "./services/auth.service";
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"],
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
+  menuItems = [
+    { routeLink: "/dashboard", icon: "dashboard", title: "Dashboard" },
+    { routeLink: "/fun-hall", icon: "event_seat", title: "Fun Hall Booking" },
+    { routeLink: "/maintenance", icon: "payment", title: "Maintenance" },
+    { routeLink: "/events", icon: "event", title: "Events Calendar" },
+    {
+      routeLink: "/fundraising",
+      icon: "volunteer_activism",
+      title: "Fund Raising",
+    },
+    { routeLink: "/elections", icon: "how_to_vote", title: "Elections" },
+    { routeLink: "/festivals", icon: "celebration", title: "Festivals" },
+    { routeLink: "/roles", icon: "admin_panel_settings", title: "Roles" },
+    { routeLink: "/complaints", icon: "report_problem", title: "Complaints" },
+    { routeLink: "/amenities", icon: "pool", title: "Amenities" },
+  ];
+
   constructor(public authService: AuthService) {}
 
   logout() {
